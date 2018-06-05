723229e9fe66cc0329a24964605c876bb8c3d804
Octavio Armenta <armenta.octavio@outlook.com>
2018-06-05 03:22:19 -0700

    UI.Desktop.JavaFX

--- Code/Tamagotchi.Presentation/Client.Desktop/src/main/java/com/outlook/octavio/armenta/App.java
+++ Code/Tamagotchi.Presentation/Client.Desktop/src/main/java/com/outlook/octavio/armenta/App.java
@@ -1,16 +1,33 @@
 package com.outlook.octavio.armenta;
 
+import com.outlook.octavio.armenta.viewmodels.HelloWorldViewModel;
+import com.outlook.octavio.armenta.views.HelloWorldView;
+import de.saxsys.mvvmfx.FluentViewLoader;
+import de.saxsys.mvvmfx.ViewTuple;
 import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class App extends Application {
-    public static void main(String... args) {
+    public static void main(String...args){
         Application.launch(args);
     }
 
     @Override
-    public void start(Stage stage) {
-        stage.setTitle("Tamagotchi - Developer Edition");
+    public void start(Stage stage){
+        stage.setTitle("Tamagotchi - Developer Edition 2018 ®");
+        stage.getIcons().add(new Image("faviconpng.png"));
+
+        ViewTuple<HelloWorldView, HelloWorldViewModel> viewTuple = FluentViewLoader.fxmlView(HelloWorldView.class).load();
+
+        Parent root = viewTuple.getView();
+        stage.setScene(new Scene(root));
+
+        stage.setMinHeight(720);
+        stage.setMinWidth(1280);
+
         stage.show();
     }
 }
