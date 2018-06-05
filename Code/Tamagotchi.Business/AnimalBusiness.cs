9a651e14e866b16afa39a64fd7dff9041f987701
Octavio Armenta <armenta.octavio@outlook.com>
2018-06-05 14:08:54 -0700

    Add AnimalBusiness; Business created but not Implemented

---
+++ Code/Tamagotchi.Business/AnimalBusiness.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tamagotchi.Business.Interfaces;
+using Tamagotchi.DataAccess.DALs.Interfaces;
+using Tamagotchi.Common.Entities;
+using Tamagotchi.Common.Models;
+
+namespace Tamagotchi.Business
+{
+    public class AnimalBusiness : BaseBusiness<AnimalModel, Animal>, IAnimalBusiness
+    {
+        public AnimalBusiness(IAnimalDAL baseDAL) : base(baseDAL)
+        {
+
+        }
+
+        public AnimalModel Create(Animal animal, byte[] package)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<AnimalModel> GetByUser(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AnimalModel Update(Animal animal, byte[] package)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
