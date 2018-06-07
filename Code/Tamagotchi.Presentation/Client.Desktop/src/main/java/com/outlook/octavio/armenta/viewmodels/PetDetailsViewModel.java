148c5ae9d2333eba87159d030aceae40d5d507c4
Pichardo <Jonathan.Pichardo@Staples.com>
2018-06-07 11:11:04 -0700

    Pet Details

--- Code/Tamagotchi.Presentation/Client.Desktop/src/main/java/com/outlook/octavio/armenta/viewmodels/PetDetailsViewModel.java
+++ Code/Tamagotchi.Presentation/Client.Desktop/src/main/java/com/outlook/octavio/armenta/viewmodels/PetDetailsViewModel.java
@@ -1,7 +1,224 @@
 package com.outlook.octavio.armenta.viewmodels;
 
 import de.saxsys.mvvmfx.ViewModel;
+import de.saxsys.mvvmfx.utils.commands.Action;
+import de.saxsys.mvvmfx.utils.commands.Command;
+import de.saxsys.mvvmfx.utils.commands.DelegateCommand;
+import javafx.beans.property.*;
+import javafx.collections.ObservableMap;
+import javafx.scene.image.Image;
+
+import java.io.File;
 
 
 public class PetDetailsViewModel implements ViewModel {
+
+    public static final String IDLE_BROWSE = "IDLE_BROWSE";
+    public static final String PLAY_BROWSE = "PLAY_BROWSE";
+    public static final String SLEEP_BROWSE = "SLEEP_BROWSE";
+    public static final String EAT_BROWSE = "EAT_BROWSE";
+    public static final String PACKAGE_BROWSE = "PACKAGE_BROWSE";
+
+    private ObjectProperty<Image> idleImage = new SimpleObjectProperty<>();
+    private ObjectProperty<Image> playImage = new SimpleObjectProperty<>();
+    private ObjectProperty<Image> sleepImage = new SimpleObjectProperty<>();
+    private ObjectProperty<Image> eatImage = new SimpleObjectProperty<>();
+
+    private ObjectProperty<File> packageProperty = new SimpleObjectProperty<>();
+
+    private StringProperty nameField = new SimpleStringProperty();
+    private StringProperty descriptionField = new SimpleStringProperty();
+    private StringProperty packageField = new SimpleStringProperty();
+
+    private MapProperty<String, String> dynamicProps;
+
+    private Command idleBrowseCommand;
+    private Command playBrowseCommand;
+    private Command sleepBrowseCommand;
+    private Command eatBrowseCommand;
+    private Command packageBrowseCommand;
+    private Command addPropCommand;
+    private Command createCommand;
+
+    public void initialize() {
+
+        idleBrowseCommand = new DelegateCommand(() -> new Action() {
+            @Override
+            protected void action() throws Exception {
+                doIdleBrowse();
+            }
+        });
+
+        playBrowseCommand = new DelegateCommand(() -> new Action() {
+            @Override
+            protected void action() throws Exception {
+                doPlayBrowse();
+            }
+        });
+
+        sleepBrowseCommand = new DelegateCommand(() -> new Action() {
+            @Override
+            protected void action() throws Exception {
+                doSleepBrowse();
+            }
+        });
+
+        eatBrowseCommand = new DelegateCommand(() -> new Action() {
+            @Override
+            protected void action() throws Exception {
+                doEatBrowse();
+            }
+        });
+
+        packageBrowseCommand = new DelegateCommand(() -> new Action() {
+            @Override
+            protected void action() throws Exception {
+                doPackageBrowse();
+            }
+        });
+
+        addPropCommand = new DelegateCommand(() -> new Action() {
+            @Override
+            protected void action() throws Exception {
+                doAddProp();
+            }
+        });
+
+        createCommand = new DelegateCommand(() -> new Action() {
+            @Override
+            protected void action() throws Exception {
+                doCreate();
+            }
+        });
+
+
+    }
+
+    public void doIdleBrowse() throws Exception {
+        publish(IDLE_BROWSE);
+    }
+
+    public void doPlayBrowse() {
+        publish(PLAY_BROWSE);
+    }
+
+    public void doSleepBrowse() {
+        publish(SLEEP_BROWSE);
+    }
+
+    public void doEatBrowse() {
+        publish(EAT_BROWSE);
+    }
+
+    public void doPackageBrowse() {
+        publish(PACKAGE_BROWSE);
+    }
+
+    public void doAddProp() {
+
+    }
+
+    public void doCreate() {
+    }
+
+
+    public Image getIdleImage() {
+        return idleImage.get();
+    }
+
+    public ObjectProperty<Image> idleImageProperty() {
+        return idleImage;
+    }
+
+    public Image getPlayImage() {
+        return playImage.get();
+    }
+
+    public ObjectProperty<Image> playImageProperty() {
+        return playImage;
+    }
+
+    public Image getSleepImage() {
+        return sleepImage.get();
+    }
+
+    public ObjectProperty<Image> sleepImageProperty() {
+        return sleepImage;
+    }
+
+    public Image getEatImage() {
+        return eatImage.get();
+    }
+
+    public ObjectProperty<Image> eatImageProperty() {
+        return eatImage;
+    }
+
+    public File getPackageProperty() {
+        return packageProperty.get();
+    }
+
+    public ObjectProperty<File> packagePropertyProperty() {
+        return packageProperty;
+    }
+
+    public String getNameField() {
+        return nameField.get();
+    }
+
+    public StringProperty nameFieldProperty() {
+        return nameField;
+    }
+
+    public String getDescriptionField() {
+        return descriptionField.get();
+    }
+
+    public StringProperty descriptionFieldProperty() {
+        return descriptionField;
+    }
+
+    public String getPackageField() {
+        return packageField.get();
+    }
+
+    public StringProperty packageFieldProperty() {
+        return packageField;
+    }
+
+    public ObservableMap<String, String> getDynamicProps() {
+        return dynamicProps.get();
+    }
+
+    public MapProperty<String, String> dynamicPropsProperty() {
+        return dynamicProps;
+    }
+
+    public Command getIdleBrowseCommand() {
+        return idleBrowseCommand;
+    }
+
+    public Command getPlayBrowseCommand() {
+        return playBrowseCommand;
+    }
+
+    public Command getSleepBrowseCommand() {
+        return sleepBrowseCommand;
+    }
+
+    public Command getEatBrowseCommand() {
+        return eatBrowseCommand;
+    }
+
+    public Command getPackageBrowseCommand() {
+        return packageBrowseCommand;
+    }
+
+    public Command getAddPropCommand() {
+        return addPropCommand;
+    }
+
+    public Command getCreateCommand() {
+        return createCommand;
+    }
 }
--- Code/Tamagotchi.Presentation/Client.Desktop/src/main/java/com/outlook/octavio/armenta/views/PetDetailsView.java
+++ Code/Tamagotchi.Presentation/Client.Desktop/src/main/java/com/outlook/octavio/armenta/views/PetDetailsView.java
@@ -1,55 +1,135 @@
 package com.outlook.octavio.armenta.views;
 
+import com.google.inject.Inject;
 import com.outlook.octavio.armenta.viewmodels.PetDetailsViewModel;
 import de.saxsys.mvvmfx.FxmlView;
+import de.saxsys.mvvmfx.InjectViewModel;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
+
+import static com.outlook.octavio.armenta.viewmodels.PetDetailsViewModel.*;
 
 public class PetDetailsView implements FxmlView<PetDetailsViewModel>, Initializable {
     @FXML
     public ImageView idleImageView;
     @FXML
-    public Button idleBrowseButton;
-    @FXML
     public ImageView playImageView;
-    @FXML
-    public Button playBrowseButton;
     @FXML
     public ImageView sleepImageView;
     @FXML
-    public Button sleepBrowseButton;
-    @FXML
     public ImageView eatImageView;
-    @FXML
-    public Button eatBrowseButton;
+
     @FXML
     public TextField nameField;
     @FXML
     public TextField descriptionField;
     @FXML
     public TextField packageField;
-    @FXML
-    public Button packageBrowseButton;
-    @FXML
-    public Button addPropButton;
+
     @FXML
     public TableView dynamicPropsTable;
     @FXML
     public TableColumn dynamicPropNameColumn;
     @FXML
     public TableColumn dynamicPropValueColumn;
+
+    @FXML
+    public Button idleBrowseButton;
+    @FXML
+    public Button playBrowseButton;
+    @FXML
+    public Button sleepBrowseButton;
+    @FXML
+    public Button eatBrowseButton;
+    @FXML
+    public Button packageBrowseButton;
+    @FXML
+    public Button addPropButton;
     @FXML
     public Button createButton;
 
-    public void initialize(URL location, ResourceBundle resources) {
+    @Inject
+    private Stage stage;
 
+    @InjectViewModel
+    private PetDetailsViewModel viewModel;
+
+    public void initialize(URL location, ResourceBundle resources) {
+        idleImageView.imageProperty().bind(viewModel.idleImageProperty());
+        playImageView.imageProperty().bind(viewModel.playImageProperty());
+        sleepImageView.imageProperty().bind(viewModel.sleepImageProperty());
+        eatImageView.imageProperty().bind(viewModel.eatImageProperty());
+
+        nameField.textProperty().bindBidirectional(viewModel.nameFieldProperty());
+        descriptionField.textProperty().bindBidirectional(viewModel.descriptionFieldProperty());
+        packageField.textProperty().bindBidirectional(viewModel.packageFieldProperty());
+
+        idleBrowseButton.setOnAction(e -> viewModel.getIdleBrowseCommand().execute());
+        playBrowseButton.setOnAction
+                (e -> viewModel.getPlayBrowseCommand().execute());
+        sleepBrowseButton.setOnAction(e -> viewModel.getSleepBrowseCommand().execute());
+        eatBrowseButton.setOnAction(e -> viewModel.getEatBrowseCommand().execute());
+        packageBrowseButton.setOnAction(e -> viewModel.getPackageBrowseCommand().execute());
+        addPropButton.setOnAction(e -> viewModel.getAddPropCommand().execute());
+        createButton.setOnAction(e -> viewModel.getCreateCommand().execute());
+
+        viewModel.subscribe(IDLE_BROWSE, (s, objects) -> {
+            FileChooser.ExtensionFilter filter = new FileChooser.ExtensionFilter("Images", "*.jpg", "*.png");
+
+            this.openFileChooser(filter, file -> {
+                viewModel.idleImageProperty().setValue(new Image(file.toURI().toString()));
+            });
+        });
+        viewModel.subscribe(PLAY_BROWSE, (s, objects) -> {
+            FileChooser.ExtensionFilter filter = new FileChooser.ExtensionFilter("Images", "*.jpg", "*.png");
+
+            this.openFileChooser(filter, file -> {
+                viewModel.playImageProperty().setValue(new Image(file.toURI().toString()));
+            });
+        });
+        viewModel.subscribe(SLEEP_BROWSE, (s, objects) -> {
+            FileChooser.ExtensionFilter filter = new FileChooser.ExtensionFilter("Images", "*.jpg", "*.png");
+
+            this.openFileChooser(filter, file -> {
+                viewModel.sleepImageProperty().setValue(new Image(file.toURI().toString()));
+            });
+        });
+        viewModel.subscribe(EAT_BROWSE, (s, objects) -> {
+            FileChooser.ExtensionFilter filter = new FileChooser.ExtensionFilter("Images", "*.jpg", "*.png");
+
+            this.openFileChooser(filter, file -> {
+                viewModel.eatImageProperty().setValue(new Image(file.toURI().toString()));
+            });
+        });
+        viewModel.subscribe(PACKAGE_BROWSE, (s, objects) -> {
+            FileChooser.ExtensionFilter filter = new FileChooser.ExtensionFilter("Animal Package", "*.dll");
+
+            this.openFileChooser(filter, file -> {
+                viewModel.packagePropertyProperty().setValue(file);
+                viewModel.packageFieldProperty().setValue(file.getName());
+            });
+        });
     }
+
+    private void openFileChooser(FileChooser.ExtensionFilter filter, Consumer<File> callback) {
+
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(filter);
+        File file = fileChooser.showOpenDialog(stage);
+        callback.accept(file);
+    }
+
 }
