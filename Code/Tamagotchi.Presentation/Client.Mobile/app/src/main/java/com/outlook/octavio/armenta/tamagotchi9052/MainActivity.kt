6c603ae19417a462023417cbd0576a9a321d1042
Octavio Armenta <armenta.octavio@outlook.com>
2018-06-11 06:21:55 -0700

    MainView

--- Code/Tamagotchi.Presentation/Client.Mobile/app/src/main/java/com/outlook/octavio/armenta/tamagotchi9052/MainActivity.kt
+++ Code/Tamagotchi.Presentation/Client.Mobile/app/src/main/java/com/outlook/octavio/armenta/tamagotchi9052/MainActivity.kt
@@ -3,7 +3,10 @@ package com.outlook.octavio.armenta.tamagotchi9052
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
 import android.view.MenuItem
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener  {
 
@@ -15,6 +18,69 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        when (item.itemId) {
+            R.id.nav_first_fragment -> {
+                Toast.makeText(this, "PET ONE", Toast.LENGTH_LONG).show()
+            }
+            R.id.nav_second_fragment -> {
+                Toast.makeText(this, "PET TWO", Toast.LENGTH_LONG).show()
+            }
+            R.id.nav_third_fragment -> {
+                Toast.makeText(this, "PET THREE", Toast.LENGTH_LONG).show()
+            }
+            R.id.nav_fourth_fragment -> {
+                Toast.makeText(this, "PET FOURTH", Toast.LENGTH_LONG).show()
+            }
+            R.id.nav_send -> {
+                Toast.makeText(this, "OPEN STORE", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
+
+/*
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+
+        val toggle = ActionBarDrawerToggle(
+                this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.test, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        when (item.itemId) {
+            R.id.action_settings -> return true
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+ */
