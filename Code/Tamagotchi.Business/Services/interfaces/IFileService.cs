e6063d0ad1ca20676900fe5bd05ec0d1d19dc383
J. Pichardo <j.pichardo@tuta.io>
2018-06-12 18:27:29 -0700

    Stuff

---
+++ Code/Tamagotchi.Business/Services/interfaces/IFileService.cs
@@ -0,0 +1,11 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace Tamagotchi.Business.Services
+{
+    public interface IFileService
+    {
+        Task SaveFile(string fileName, string content);
+        Task GetFile(string fileName);
+    }
+}
