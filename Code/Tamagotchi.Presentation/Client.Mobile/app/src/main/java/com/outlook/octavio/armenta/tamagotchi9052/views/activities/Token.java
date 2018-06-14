2eabe4be71df6bc9477ef57b29f3cc4f0a7db7ea
Octavio Armenta <armenta.octavio@outlook.com>
2018-06-14 14:52:43 -0700

    test

--- Code/Tamagotchi.Presentation/Client.Mobile/app/src/main/java/com/outlook/octavio/armenta/tamagotchi9052/views/activities/Token.java
+++ Code/Tamagotchi.Presentation/Client.Mobile/app/src/main/java/com/outlook/octavio/armenta/tamagotchi9052/views/activities/Token.java
@@ -3,10 +3,20 @@ package com.outlook.octavio.armenta.tamagotchi9052;
 class Token {
     private static final Token ourInstance = new Token();
 
+    private String guid;
+
     private Token() {
     }
 
     static Token getInstance() {
         return ourInstance;
+    }
+
+    public String getGuid(){
+        return guid;
+    }
+
+    public void setGuid(String guid) {
+        this.guid = guid;
     }
 }
